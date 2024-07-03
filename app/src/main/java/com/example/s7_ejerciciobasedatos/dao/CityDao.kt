e83735e353f5824873c331ce1a87ade14fde7225
CEPS0501 <christianepasquierino@yahoo.com>
2024-07-03 13:36:10 -0600

    Initial commit

---
+++ app/src/main/java/com/example/s7_ejerciciobasedatos/dao/CityDao.kt
@@ -0,0 +1,25 @@
+package com.example.s7_ejerciciobasedatos.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.s7_ejerciciobasedatos.entities.City
+
+@Dao
+abstract class CityDao {
+    @Insert
+    abstract suspend fun insert(city: City)
+
+    @Query("select * from cities")
+    abstract suspend fun getAllcities():List<City>
+
+    @Update
+    abstract suspend fun update(city: City)
+
+    @Delete
+    abstract suspend fun delete(city: City)
+
+
+}
